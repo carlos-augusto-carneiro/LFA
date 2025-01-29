9b7391f2e186537eec909d0247d610b004dbfaf7
carlos-augusto-carneiro <devcarlosaugusto@outlook.com>
2025-01-29 12:06:38 -0300

    Alteração

--- main.py
+++ main.py
@@ -11,21 +11,21 @@ if __name__ == "__main__":
     Test.calc('a*a+(a+a)')
     Test.calc('a+(')
 
-    print("\n"+"\033[43m" + "=" * 40 + "\033[0m")  # Sinais de igual em amarelo
-    print("\033[36m" + "Número Binário Múltiplos de 3" + "\033[0m")  # Texto em ciano
-    print("\033[43m" + "=" * 40 + "\033[0m"+"\n")  # Sinais de igual em amarelo
+    print("\n"+"\033[43m" + "=" * 40 + "\033[0m")  
+    print("\033[36m" + "Número Binário Múltiplos de 3" + "\033[0m") 
+    print("\033[43m" + "=" * 40 + "\033[0m"+"\n")  
     Test.teste_y_x('00001111')
     Test.teste_y_x('00001110')
 
-    print("\n" + "\033[43m" + "=" * 40 + "\033[0m")  # Sinais de igual em amarelo
-    print("\033[36m" + "Reverso" + "\033[0m")  # Texto "Reverso" em ciano
-    print("\033[43m" + "=" * 40 + "\033[0m"+"\n")  # Sinais de igual em amarelo
+    print("\n" + "\033[43m" + "=" * 40 + "\033[0m")  
+    print("\033[36m" + "Reverso" + "\033[0m")  
+    print("\033[43m" + "=" * 40 + "\033[0m"+"\n")  
     Test.reverso('1001001001')
     Test.reverso('10101010101')
 
-    print("\n"+"\033[43m" + "=" * 40 + "\033[0m")  # Caixa amarela
-    print("\033[36m" + "While" + "\033[0m")  # Texto "While" em ciano
-    print("\033[43m" + "=" * 40 + "\033[0m"+"\n")  # Caixa amarela
+    print("\n"+"\033[43m" + "=" * 40 + "\033[0m")  
+    print("\033[36m" + "While" + "\033[0m")  
+    print("\033[43m" + "=" * 40 + "\033[0m"+"\n")  
     Test.while_grammar('eqt(a){eqt(a){}}')
     Test.while_grammar('eqt(a){eqt(a){eqt(a){eqt(a){eqt(a){eqt(a){eqt(a){eqt(a){eqt(a){eqt(a){eqt(a){eqt(a){eqt(a){eqt(a){eqt(a){eqt(a){}}}}}}}}}}}}}}}}')
     Test.while_grammar('eqt(a){eqt(a){}')
